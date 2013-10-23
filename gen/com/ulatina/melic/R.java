917017845043a4d042d0c6ccee5c181a943eb69c
cheka2893 <siba2893@gmail.com>
2013-10-22 21:15:50 -0600

    Graphic UI

--- gen/com/ulatina/melic/R.java
+++ gen/com/ulatina/melic/R.java
@@ -21,27 +21,52 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int fblogo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Button01=0x7f08000a;
+        public static final int Button02=0x7f080009;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int TextView01=0x7f080008;
+        public static final int action_settings=0x7f08000d;
+        public static final int button1=0x7f080007;
+        public static final int editText1=0x7f080001;
+        public static final int imageView1=0x7f080005;
+        public static final int spinner1=0x7f08000b;
+        public static final int spinner2=0x7f08000c;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080003;
+        public static final int textView4=0x7f080004;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_taxi_request=0x7f030002;
+        public static final int activity_main_menu=0x7f030002;
+        public static final int activity_taxi_request=0x7f030003;
     }
     public static final class menu {
         public static final int login=0x7f070000;
         public static final int main=0x7f070001;
-        public static final int taxi_request=0x7f070002;
+        public static final int main_menu=0x7f070002;
+        public static final int taxi_request=0x7f070003;
     }
     public static final class string {
+        public static final int RequestButton=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_login=0x7f050003;
-        public static final int title_activity_taxi_request=0x7f050004;
+        public static final int coopSelection=0x7f050008;
+        public static final int loginButton=0x7f050006;
+        public static final int login_exception_txt=0x7f050005;
+        public static final int login_username=0x7f050004;
+        public static final int menu_central_request=0x7f05000c;
+        public static final int menu_log_out=0x7f05000d;
+        public static final int menu_taxi_request=0x7f05000b;
+        public static final int specialTaxiSpin=0x7f050009;
+        public static final int title_activity_login=0x7f050002;
+        public static final int title_activity_main_menu=0x7f05000a;
+        public static final int title_activity_taxi_request=0x7f050003;
     }
     public static final class style {
         /** 
